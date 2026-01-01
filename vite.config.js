1235465fbdfb17748a76b0baa6ab3002dec1f8ec
Jeremy O. Rellama <113158566+PaperNsalt@users.noreply.github.com>
2026-01-01 22:25:24 +0800

    Update project paths and homepage for GWA portfolio
    Renamed favicon path in index.html and moved the logo SVG to the public directory. Updated the homepage and base path in package.json and vite.config.js to reflect the new project name 'bu-gwa-portfolio'.

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/bu-react-portfolio/",
+  base: '/bu-gwa-portfolio/',
 })
